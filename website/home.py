b6d34c21cac5a9fda6d835793dfa477b2d22d106
NAVNEET SINGH <74829666+its-me-navee@users.noreply.github.com>
2023-12-22 01:14:45 +0530

    Update home.py

--- website/home.py
+++ website/home.py
@@ -50,4 +50,4 @@ def homepage():
             print("Not found")
             return render_template("error.html", name=movie_name)
     else:
-        return render_template("home.html")
+        return render_template("index.html")
